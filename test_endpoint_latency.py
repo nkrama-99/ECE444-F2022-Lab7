21f0e57535f64e30c6e87ed0e3ac90baa003e617
Ramakrishna Natarajan <nkrama99@gmail.com>
2022-11-28 22:52:34 -0500

    average latency

--- test_endpoint_latency.py
+++ test_endpoint_latency.py
@@ -1,5 +1,21 @@
 import requests
+from datetime import datetime
 
-response = requests.post('http://ece444-lab7-v1-env.us-east-2.elasticbeanstalk.com/predict', json = {'text':'The ocean is big'})
-response = response.json()
-print(response['prediction'])
+results = []
+
+for i in range(100):
+    startTime = datetime.now()
+    response = requests.post('http://ece444-lab7-v1-env.us-east-2.elasticbeanstalk.com/predict', json = {'text':'The ocean is big'})
+    response = response.json()
+    endTime = datetime.now()
+    delta = (endTime - startTime).total_seconds() * 1000
+    
+    if (response['prediction'] == "REAL"):
+        results.append(delta)
+        print(i, "Response time:", delta, "ms")
+    else:
+        print("unexpected failure")
+        
+averageLatency = sum(results) / len(results)
+print("Number of requests made:", len(results))
+print("Average latency:", averageLatency)
